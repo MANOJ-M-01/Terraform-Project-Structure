2c594a980a0f28f0dae0f9458ba90af863f1b9d0
manoj-m-01 <manoj904378@gmail.com>
2024-07-21 17:10:44 +0530

    using data from another service added

--- v1/dev/subnet/main.tf
+++ v1/dev/subnet/main.tf
@@ -1,15 +1,20 @@
-# resource "aws_subnet" "private" {
-#   vpc_id     = aws_vpc.main.id
-#   cidr_block = var.subnet_cidr
-#   tags = {
-#     Name = "${var.env}-subnet"
-#   }
-# }
-
-
-data "terraform_remote_state" "vpc"{
-  backend = "local"
-  config = {
-    path = "../vpc/.terraform.tfstate"
+resource "aws_subnet" "private" {
+  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
+  cidr_block = var.subnet_cidr
+  tags = {
+    Name = "${var.env}-subnet"
   }
 }
+
+
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+  config = {
+    path = "../vpc/terraform.tfstate"
+  }
+}
+
+# output "vpc" {
+#   description = "vpc details"
+#   value       = data.terraform_remote_state.vpc
+# }
