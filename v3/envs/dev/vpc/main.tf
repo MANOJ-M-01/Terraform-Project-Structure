e4e616ec7a5cc9e75efb179c7fc591e33e740d60
manoj-m-01 <manoj904378@gmail.com>
2024-07-21 19:16:41 +0530

    vpc 0.1.2 updated

--- v3/envs/dev/vpc/main.tf
+++ v3/envs/dev/vpc/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 module "vpc" {
 
   # source = "git@github.com:MANOJ-M-01/Terraform-Module-AWS_VPC.git?ref=0.1.0"
-  source = "github.com/MANOJ-M-01/Terraform-Module-AWS_VPC?ref=0.1.0"
+  source = "github.com/MANOJ-M-01/Terraform-Module-AWS_VPC?ref=0.1.2"
 
   env        = "dev"
   cidr_block = "10.0.0.0/16"
