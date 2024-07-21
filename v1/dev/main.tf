7f2a1e9ede40d03067ce979408b9a3b69a9d95a0
manoj-m-01 <manoj904378@gmail.com>
2024-07-21 16:44:14 +0530

    dev structure added

--- v1/dev/main.tf
+++ v1/dev/main.tf
@@ -1,16 +1,10 @@
-provider "aws" {
-  profile = var.profile
-  region  = var.region
-}
-
-
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = false
   enable_dns_hostnames = false
 
   tags = {
-    Name = "dev-main"
+    Name = "${var.env}-main"
   }
 }
 
