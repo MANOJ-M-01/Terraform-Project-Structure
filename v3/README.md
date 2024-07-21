74ce724c72e17c0a1cfb4eace5467dd582e34f2a
manoj-m-01 <manoj904378@gmail.com>
2024-07-21 19:04:00 +0530

    remote module with vpc added

---
+++ v3/README.md
@@ -0,0 +1,3 @@
+
+### Module Repo
+https://github.com/MANOJ-M-01/Terraform-Module-AWS_VPC
---
+++ v3/envs/dev/vpc/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "manoj_admin"
+  region  = var.region
+}
+
+
+module "vpc" {
+
+  # source = "git@github.com:MANOJ-M-01/Terraform-Module-AWS_VPC.git?ref=0.1.0"
+  source = "github.com/MANOJ-M-01/Terraform-Module-AWS_VPC?ref=0.1.0"
+
+  env        = "dev"
+  cidr_block = "10.0.0.0/16"
+}
